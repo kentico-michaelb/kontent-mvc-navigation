2de5701ec96dccb2e9404d05b6f3c7de26bf7a82
MichaelB <MichaelB@kentico.com>
2021-02-01 11:32:38 -0500

    Language selector Spanish to English bug fix

--- Kontent MVC Navigation/Models/LanguageSwitcher.cs
+++ Kontent MVC Navigation/Models/LanguageSwitcher.cs
@@ -10,8 +10,6 @@ namespace Kontent_MVC_Navigation.Models
     {
         public CultureInfo CurrentUICulture { get; set; }
         public List<CultureInfo> SupportedCultures { get; set; }
-        public string CurrentController { get; set; }
-        public string CurrentAction { get; set; }
-        public string CurrentPath { get; set; }
+        public IEnumerable<LanguageSwitcherOption> CultureOptions { get; set; }
     }
 }
